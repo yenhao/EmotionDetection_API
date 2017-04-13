b479a40340473d07b34fcb647c56178c88a8aced
yenhao <yenhao0218@gmail.com>
2017-04-13 15:54:22 +0800

    change example and remove unneed lib

--- EmotionDetection_ch.py
+++ EmotionDetection_ch.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-import requests
 import json
 import urllib2
 
@@ -34,8 +33,12 @@ class EmotionDetection(object):
 if __name__ == '__main__':
     em = EmotionDetection()
 
-    texts_list = ['好無聊喔！','卡提諾狂新聞666']
+    texts_list = ['Apple不能作業系統阿Apple是IOS系統','看不到你的號碼因為我們這樣分析的','好的那歡迎你直到了晚上去體驗看看','4分鐘不只阿他他他覺得這樣速度很慢']
 
     json = em.get_obj(texts_list)
 
-    print(json['data'])
+    for js_obj in json['data']:
+        output_format = '{}\nFirst Emotion : {}\nSecond Emotion : {}\nIs it Ambiguous : {}\n\n'.format(
+            (js_obj.get('message')).encode('utf-8'), js_obj.get('emotion1'), js_obj.get('emotion2'), js_obj.get('ambiguous')
+            )
+        print(output_format)
